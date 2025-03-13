8c1c6cfa50962bf73e15bda9508d1bd53b6db4a0
preethika143 <rpreethi2304@gmail.com>
2025-03-13 15:07:00 +0530

    robinson

---
+++ king15.java
@@ -0,0 +1,16 @@
+public class king15
+{
+    public static void main(String[]args)
+    {
+        int a=6;
+        if(a<=18)
+        {
+            System.out.println("you are not eligible for vote");
+        }
+        else
+        {
+            System.out.println("you are  eligible for  vote");
+        }
+ }
+
+}

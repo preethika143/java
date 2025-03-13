be0aab4adde4aebcabc847aab568c024b2e52544
preethika143 <rpreethi2304@gmail.com>
2025-03-13 15:23:09 +0530

    zinuu

---
+++ king16.java
@@ -0,0 +1,18 @@
+public class king15
+{
+    public static void main(String[]args)
+    {
+        int a=90
+        ;
+        if(a<=18)
+        {
+            System.out.println("you are not eligible for vote");
+        }
+        else
+        if(a>=18)
+        {
+            System.out.println("you are  eligible for  vote");
+        }
+ }
+
+}

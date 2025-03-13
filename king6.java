16cb956396f6265b90bd459b2a6ccf0031c26bb8
preethika143 <rpreethi2304@gmail.com>
2025-03-13 09:59:18 +0530

    queen

---
+++ king6.java
@@ -0,0 +1,13 @@
+
+
+public class king6{
+    public static void main(String[]args)
+{
+    String name="preethika";
+    char grade='a';
+    int mark=100;
+    System.out.println("enter the name:"+name);
+    System.out.println("enter the grade:"+grade);
+    System.out.println("enter the mark:"+mark);
+    }
+}

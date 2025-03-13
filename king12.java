91f59cd3d2c017827631d60621df98bd7adf062e
preethika143 <rpreethi2304@gmail.com>
2025-03-13 13:40:36 +0530

    rohini

---
+++ king12.java
@@ -0,0 +1,14 @@
+public class king12
+{
+    public static void main(String[] args)
+     {
+        int j=9;
+        int k=5;
+        j=j+k;
+        k=j-k;
+        j=j-k;
+        System.out.println(k);
+        System.out.println(j) ;       
+        
+    }
+}

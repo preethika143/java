5f307b87338eeaf9328d49bf6e2adece8cb2d872
preethika143 <rpreethi2304@gmail.com>
2025-03-13 11:35:04 +0530

    princess

---
+++ king9.java
@@ -0,0 +1,12 @@
+public class king9
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=30;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+
+    }
+}

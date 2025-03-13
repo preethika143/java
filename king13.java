f1d73fe36459153aa3e38c18dba0f8b64952c987
preethika143 <rpreethi2304@gmail.com>
2025-03-13 14:18:07 +0530

    kutty

---
+++ king13.java
@@ -0,0 +1,10 @@
+public class king13
+{
+    public static void main(String[]args)
+    {
+        String k=("i am your best friend");
+        String d="i love briyani";
+        System.out.println("length of the string:"+k.length());
+        System.out.println("length of the string:"+d.length());
+    }
+}

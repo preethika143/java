dfe92efa230247dc98a726be3ee8b451f0baab32
preethika143 <rpreethi2304@gmail.com>
2025-03-13 11:48:28 +0530

    briyani

---
+++ king10.java
@@ -0,0 +1,17 @@
+public class king10
+{
+    public static void main(String[] args) {
+        {
+            String name1="robinson";
+            String name2="sobika";
+            int k=10;
+            int h=9;
+            double u=3.456;
+            double o=7.8999;
+            System.out.println(o!=u);
+            System.out.println(name1!=name2);
+            System.out.println(name1==name2);
+            System.out.println(k>h);
+        }
+    }
+}

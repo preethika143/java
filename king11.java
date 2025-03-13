6659ff3a2b7cf61a6484e73493e97f88dc9c79de
preethika143 <rpreethi2304@gmail.com>
2025-03-13 11:59:07 +0530

    hello queen

---
+++ king11.java
@@ -0,0 +1,11 @@
+public class king10{
+    public static void main(String[]args)
+    {
+        int a=65;
+        int b=76;
+        int h=65;
+        int f=34;
+        System.out.println(a==h&&h==b);
+        System.out.println(a!=f||a>h);
+    }
+}

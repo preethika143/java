a9108c04893012be0afe9a6749b4ad36993726a1
preethika143 <rpreethi2304@gmail.com>
2025-03-14 13:48:28 +0530

    mobile

---
+++ king26.java
@@ -0,0 +1,10 @@
+public class king26
+{
+    public static void main(String[] args) {
+
+     String [] friends={"preethi","pavi","mubetha","savitha","joo","zinu","ragul","rani"} ;
+     int [] mark={100,99,98,97,96,67,56};
+     System.out.println(friends[5]);
+     System.out.println(mark[3]);
+    }
+}

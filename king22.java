cf8c3217ed9672591fa3f3b6d834cfbf9f4c4588
preethika143 <rpreethi2304@gmail.com>
2025-03-14 11:09:28 +0530

    oviyaa

---
+++ king22.java
@@ -0,0 +1,10 @@
+public class king22
+{
+    public static void main(String[] args) {
+        int i=1;
+         {
+            for(i=1;i<=5;i++)
+                System.out.println("oviyaa");
+    }
+    }
+}

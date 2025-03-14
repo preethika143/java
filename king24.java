cd1f6e871410721499040a0fa85dffa383724013
preethika143 <rpreethi2304@gmail.com>
2025-03-14 13:27:39 +0530

    raguleyii

---
+++ king24.java
@@ -0,0 +1,10 @@
+public class king24
+{
+     static void time(){
+        System.out.println("i love my self");
+        
+    }
+    public static void main(String[] args) {
+        time();
+    }
+}

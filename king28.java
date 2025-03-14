456dbb9fa54555045aab3556754909432caca536
preethika143 <rpreethi2304@gmail.com>
2025-03-14 14:14:23 +0530

    friendship

---
+++ king28.java
@@ -0,0 +1,9 @@
+public class king28
+{
+    public static void main(String[] args) {
+    String [][] friends={{"pavi","mubetha","savitha","zinu","ragul"},{"love","care","enemy","marrige","crush"}};
+     int [][] mark={{100,99,98,97,96,67,56},{1,2,3,4,5,6,7}};
+     System.out.println(mark[1][5]);
+     System.out.println(friends[1][3]);
+      }
+}
